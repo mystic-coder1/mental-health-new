d4e1eb366e80a39eb05326d2ecff5e885257647e
Pushpit Upadhyay <coderpushpit@gmail.com>
2025-09-20 01:06:43 +0530

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import Homepage from './assets/homepage';
+
 import StudentLogin from './assets/studentlogin';
-// import Category from './pages/category';
-// import Community from './pages/communitypage';
-// import DoctorLogin from './pages/logindoctor';
-// import Counsellor from './pages/counsellor';
-// import Chatbot from './pages/chatbot';
-// import StudentAccount from './pages/studentaccount';
-// import AR from './pages/ARpage';
-// import BeforeLogin from './pages/beforelogin';
-// import Session from './pages/sessionwithstudent';
+
 
 
 
@@ -19,8 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to="/student-login" replace />} />
       <Route path="/student-login" element={<StudentLogin />} />
-      <Route path="/homepage" element={<Homepage />} />
-      {/* future routes go here */}
+    
       <Route path="*" element={<Navigate to="/student-login" replace />} />
     </Routes>
   );
